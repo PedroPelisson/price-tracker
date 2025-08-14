dea09e7fda6b077c34272d4290de80c681f40a49
Pedro Henrique Pelisson <pedrohenriquepelisson@MacBook-Air-de-Pedro.local>
2025-08-14 00:18:35 -0300

    feat: Create Product data model and initial migration

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_url = models.URLField(unique=True)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    desired_price = models.DecimalField(max_digits=10, decimal_places=2)
+    last_update = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
+
+    def __str__(self):
+        return self.name
